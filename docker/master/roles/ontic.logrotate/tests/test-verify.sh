76fb5477cddb7d2fca7716f8e6cf11392d9344ad
Sameer <msameerbutt@gmail.com>
2020-11-13 01:15:27 +1100

    root user is installed in worker node

--- docker/master/roles/ontic.logrotate/tests/test-verify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
-# See the COPYING file bundled with this package for license details.
-
-# Verify the installed location.
-docker exec --tty ${container_id} env TERM=xterm which logrotate
-# Verify the installed version.
-docker exec --tty ${container_id} env TERM=xterm logrotate --version
